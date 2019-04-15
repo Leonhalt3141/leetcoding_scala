b11d3d7d95da70bbea44400e83bec4998d8bab1e
Ken Kuwata <ken@farmx.co>
2019-04-15 00:33:30 -0700

    [add]2 algorithm solving two sum

---
+++ src/main/scala/two_sum.scala
@@ -0,0 +1,36 @@
+
+object two_sum {
+  /*
+    https://leetcode.com/problems/two-sum/
+  */
+
+  def twosum(nums: Array[Int], target: Int): Array[Int] = {
+    for (i <- nums.indices) {
+      val answer = target - nums(i)
+      for (j <- i + 1 until nums.length) {
+        if ( answer == nums(j)) {
+          return Array(i, j)
+        }
+      }
+    }
+    throw new Exception("index not found")
+  }
+
+  def twosum2(nums: Array[Int], target: Int): Array[Int] = {
+    val withIndex = nums.zipWithIndex
+    val hashmap = withIndex.toMap
+    withIndex.collectFirst {
+      case (value, index) if hashmap.get(target - value).exists(_ != index) => Array(index, hashmap(target-value))
+    }.get
+  }
+
+  def main(args: Array[String]): Unit = {
+    val nums = Array(3, 3)
+    val target = 6
+
+    val result = twosum2(nums, target)
+    println(result.mkString(", "))
+
+  }
+
+}
