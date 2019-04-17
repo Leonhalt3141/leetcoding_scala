91634e29955c7c1a4a9d917748d77666fe438a6f
Ken Kuwata <ken@farmx.co>
2019-04-17 00:53:57 -0700

    [add]3Sum

---
+++ src/main/scala/three_sum/three_sum.scala
@@ -0,0 +1,17 @@
+/*
+  https://leetcode.com/problems/3sum/
+ */
+
+package three_sum
+
+object three_sum {
+  def threeSum(nums: Array[Int]): List[List[Int]] = {
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val nums = Array(-1, 0, 1, 2, -1, -4)
+
+    val result = threeSum(nums)
+  }
+}
