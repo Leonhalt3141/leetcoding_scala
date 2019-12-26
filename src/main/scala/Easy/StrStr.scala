06d1ac4216f8bedb686f47e12af7cdafe668ac9c
Ken Kuwata <ken@farmx.co>
2019-12-26 11:59:53 -0800

    [add]Implement strStr()

---
+++ src/main/scala/Easy/StrStr.scala
@@ -0,0 +1,16 @@
+package Easy
+
+object StrStr {
+  /*
+    EASY
+    https://leetcode.com/problems/implement-strstr/
+    Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+    Input: haystack = "hello", needle = "ll"
+    Output: 2
+
+    Input: haystack = "aaaaa", needle = "bba"
+    Output: -1
+   */
+
+
+}
