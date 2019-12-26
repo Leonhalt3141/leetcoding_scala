40881b3561f7513e6f5ec00db92318ab894157ad
Ken Kuwata <ken@farmx.co>
2019-12-26 12:44:47 -0800

    [add]Search Insert Position

---
+++ src/main/scala/Easy/SearchInsertPosition.scala
@@ -0,0 +1,26 @@
+package Easy
+
+object SearchInsertPosition {
+  /*
+    EASY
+    https://leetcode.com/problems/search-insert-position/
+    Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+    Input: [1,3,5,6], 5
+    Output: 2
+
+    Input: [1,3,5,6], 2
+    Output: 1
+
+    Input: [1,3,5,6], 7
+    Output: 4
+
+    Input: [1,3,5,6], 0
+    Output: 0
+
+
+   */
+
+  def searchInsert(nums: Array[Int], target: Int): Int = {
+
+  }
+}
