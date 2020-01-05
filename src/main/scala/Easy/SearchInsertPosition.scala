aa55be3998dbda069e4e8c1536376739b0789b45
Kentaro Kuwata <kuwaken0925@gmail.com>
2020-01-05 00:47:23 -0800

    [add]

--- src/main/scala/Easy/SearchInsertPosition.scala
+++ src/main/scala/Easy/SearchInsertPosition.scala
@@ -19,8 +19,51 @@ object SearchInsertPosition {
 
 
    */
+  def searchInsert2(nums: Array[Int], target: Int): Int = {
+    if (nums == null || nums.isEmpty) return 0
+    go(nums, 0, nums.length - 1, target)
+  }
+
+  private def go(nums: Array[Int], start: Int, end: Int, target: Int): Int = {
+    val mid = start + (end - start) / 2
+    if (end < start || nums(mid) == target) mid
+    else if (nums(mid) < target) go(nums, mid + 1, end, target)
+    else go(nums, start, mid - 1, target)
+  }
 
   def searchInsert(nums: Array[Int], target: Int): Int = {
+    if (nums.isEmpty || target <= nums.head) return 0
+    else if (target > nums.last) return nums.length
 
+    var index = 0
+    for (num_range <- nums.sliding(3).toArray) {
+      if (target <= num_range.head) return index
+      else if (num_range.head < target && target <= num_range(1)) return index + 1
+      else if (target <= num_range.last) return index + 2
+      else index += 1
+    }
+    index + 2
+  }
+
+  def main(args: Array[String]): Unit = {
+    val nums1 = Array(1, 3, 5, 6)
+    val target1 = 5
+    println(searchInsert(nums1, target1))
+
+    val nums2 = Array(1,3,5,6)
+    val target2 = 2
+    println(searchInsert(nums2, target2))
+
+    val nums3 = Array(1,3,5,6)
+    val target3 = 7
+    println(searchInsert(nums3, target3))
+
+    val nums4 = Array(1,3,5,6)
+    val target4 = 0
+    println(searchInsert(nums4, target4))
+
+    val nums5 = Array(1, 3)
+    val target5 = 4
+    println(searchInsert(nums5, target5))
   }
 }
--- src/main/scala/Easy/StrStr.scala
+++ src/main/scala/Easy/StrStr.scala
@@ -12,5 +12,42 @@ object StrStr {
     Output: -1
    */
 
+  def strStr2(haystack: String, needle: String): Int = {
+    if (needle.isEmpty) return 0
+    val needle_length = needle.length
+    val n = haystack.length
+
+    for (i <- 0 until n) {
+      val end_index = i + needle_length
+      if (end_index > n) return -1
+      else {
+        val str = haystack.substring(i, end_index)
+        if (str == needle) {
+          return i
+        }
+      }
+    }
+    -1
+  }
+
+  def strStr(haystack: String, needle: String): Int = {
+    if (needle.isEmpty) return 0
+    val needle_length = needle.length
+
+    for ((str, index) <- haystack.sliding(needle_length).zipWithIndex) {
+      if (str matches needle) return index
+    }
+    -1
+  }
+
+  def main(args: Array[String]): Unit = {
+    val haystack1 = "hello"
+    val needle1 = "ll"
+    println(strStr2(haystack1, needle1))
+
+    val haystack2 = "aaaaa"
+    val needle2 = "bba"
+    println(strStr2(haystack2, needle2))
+  }
 
 }
